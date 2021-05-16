36218ea0cdb3f89d01696b2700c3bab8fe2fd3c5
j-kyoda <j-kyoda@gmail.com>
2021-05-16 21:30:41 +0900

    Change install version to v14.17.0

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,21 +3,21 @@
 
 # install nodejs
 cd /usr/local/src
-curl -O -L https://nodejs.org/dist/v14.15.4/node-v14.15.4-linux-x64.tar.xz
-tar Jxf node-v14.15.4-linux-x64.tar.xz
+curl -O -L https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz
+tar Jxf node-v14.17.0-linux-x64.tar.xz
 cd /usr/local/bin
 if [ -e node ]
 then
     rm -f node
 fi
-ln -s /usr/local/src/node-v14.15.4-linux-x64/bin/node node
+ln -s /usr/local/src/node-v14.17.0-linux-x64/bin/node node
 if [ -e npm ]
 then
     rm -f npm
 fi
-ln -s /usr/local/src/node-v14.15.4-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
+ln -s /usr/local/src/node-v14.17.0-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
 if [ -e npx ]
 then
     rm -f npx
 fi
-ln -s /usr/local/src/node-v14.15.4-linux-x64/lib/node_modules/npm/bin//npx-cli.js npx
+ln -s /usr/local/src/node-v14.17.0-linux-x64/lib/node_modules/npm/bin//npx-cli.js npx
