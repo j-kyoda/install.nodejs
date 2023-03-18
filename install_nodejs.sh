8bc6866caa4ac97927d1ae7fd915fe681c5b0920
j-kyoda <j.kyoda@gmail.com>
2023-03-19 00:09:58 +0900

    Change install version to v18.15.0

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,21 +3,21 @@
 
 # install nodejs
 cd /usr/local/src
-curl -O -L https://nodejs.org/dist/v18.14.1/node-v18.14.1-linux-x64.tar.xz
-tar Jxf node-v18.14.1-linux-x64.tar.xz
+curl -O -L https://nodejs.org/dist/v18.15.0/node-v18.15.0-linux-x64.tar.xz
+tar Jxf node-v18.15.0-linux-x64.tar.xz
 cd /usr/local/bin
 if [ -e node ]
 then
     rm -f node
 fi
-ln -s /usr/local/src/node-v18.14.1-linux-x64/bin/node node
+ln -s /usr/local/src/node-v18.15.0-linux-x64/bin/node node
 if [ -e npm ]
 then
     rm -f npm
 fi
-ln -s /usr/local/src/node-v18.14.1-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
+ln -s /usr/local/src/node-v18.15.0-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
 if [ -e npx ]
 then
     rm -f npx
 fi
-ln -s /usr/local/src/node-v18.14.1-linux-x64/lib/node_modules/npm/bin/npx-cli.js npx
+ln -s /usr/local/src/node-v18.15.0-linux-x64/lib/node_modules/npm/bin/npx-cli.js npx
