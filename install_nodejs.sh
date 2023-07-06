087e4f58cb8f460b3d8489ee5752168ec51af343
j-kyoda <j.kyoda@gmail.com>
2023-07-06 20:27:09 +0900

    Change install version to v18.16.1

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
+VER_STR="v18.16.1"
+
 # install nodejs
 cd /usr/local/src
-curl -O -L https://nodejs.org/dist/v18.16.0/node-v18.16.0-linux-x64.tar.xz
-tar Jxf node-v18.16.0-linux-x64.tar.xz
+curl -O -L https://nodejs.org/dist/${VER_STR}/node-${VER_STR}-linux-x64.tar.xz
+tar Jxf node-${VER_STR}-linux-x64.tar.xz
 cd /usr/local/bin
 if [ -e node ]
 then
     rm -f node
 fi
-ln -s /usr/local/src/node-v18.16.0-linux-x64/bin/node node
+ln -s /usr/local/src/node-${VER_STR}-linux-x64/bin/node node
 if [ -e npm ]
 then
     rm -f npm
 fi
-ln -s /usr/local/src/node-v18.16.0-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
+ln -s /usr/local/src/node-${VER_STR}-linux-x64/lib/node_modules/npm/bin/npm-cli.js npm
 if [ -e npx ]
 then
     rm -f npx
 fi
-ln -s /usr/local/src/node-v18.16.0-linux-x64/lib/node_modules/npm/bin/npx-cli.js npx
+ln -s /usr/local/src/node-${VER_STR}-linux-x64/lib/node_modules/npm/bin/npx-cli.js npx
